75e240827a6c692d49437273604a4d2418254e39
Chris Grimble <christopher.grimble@zego.com>
2026-02-27 10:55:08 +0000

    Moved code out of controller to service

--- src/pages/group/controller.js
+++ src/pages/group/controller.js
@@ -1,19 +1,8 @@
 import { statusCodes } from '../../constants/status-codes.js'
-import { createGroup as serviceCreateGroup } from './service.js'
-import { config } from '../../config/config.js'
-import Boom from '@hapi/boom'
+import * as service from './service.js'
 
 async function getGroupsPage (_request, h) {
-  const backendRagServer = config.get('backend_rag_service')
-  const response = await fetch(`${backendRagServer}/knowledge/groups`)
-
-  if (!response.ok) {
-    const errorMessage = await response.text()
-    console.error(`Failed to fetch groups with status ${response.status}: ${errorMessage}`)
-    throw Boom.badImplementation()
-  }
-
-  const groups = await response.json()
+  const groups = await service.getGroups()
 
   return h.view('group/page.njk', { groups })
     .code(statusCodes.HTTP_STATUS_OK)
@@ -27,7 +16,7 @@ function getAddGroupForm (_request, h) {
 async function createGroup (request, h) {
   const { name, owner, description } = request.payload
 
-  const group = await serviceCreateGroup(name, owner, description)
+  const group = await service.createGroup(name, owner, description)
 
   console.info(`Created group ${name} id ${group.groupId}`)
 
@@ -46,16 +35,8 @@ function failCreateGroup (request, h, err) {
 
 async function getGroup (request, h) {
   const { groupId } = request.params
-  const backendRagServer = config.get('backend_rag_service')
-  const response = await fetch(`${backendRagServer}/knowledge/groups/${groupId}`)
 
-  if (!response.ok) {
-    const errorMessage = await response.text()
-    console.error(`Failed to fetch group ${groupId} with status ${response.status}: ${errorMessage}`)
-    throw Boom.badImplementation()
-  }
-
-  const group = await response.json()
+  const group = await service.getGroup(groupId)
 
   return h.view('group/group.njk', { group })
     .code(statusCodes.HTTP_STATUS_OK)
@@ -83,18 +64,7 @@ async function addSource (request, h) {
   const { groupId } = request.params
   const { name, type, location } = request.payload
 
-  const backendRagServer = config.get('backend_rag_service')
-  const response = await fetch(`${backendRagServer}/knowledge/groups/${groupId}/sources`, {
-    method: 'PATCH',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name, type, location })
-  })
-
-  if (!response.ok) {
-    const errorMessage = await response.text()
-    console.error(`Failed to add source with status ${response.status}: ${errorMessage}`)
-    throw Boom.badImplementation()
-  }
+  await service.addSource(groupId, name, type, location)
 
   return h.redirect(`/group/${groupId}`).code(statusCodes.HTTP_STATUS_SEE_OTHER)
 }
--- src/pages/group/service.js
+++ src/pages/group/service.js
@@ -2,8 +2,21 @@ import Boom from '@hapi/boom'
 
 import { config } from '../../config/config.js'
 
+const backendRagServer = config.get('backend_rag_service')
+
+async function getGroups () {
+  const response = await fetch(`${backendRagServer}/knowledge/groups`)
+
+  if (!response.ok) {
+    const errorMessage = await response.text()
+    console.error(`Failed to fetch groups with status ${response.status}: ${errorMessage}`)
+    throw Boom.badImplementation()
+  }
+
+  return response.json()
+}
+
 async function createGroup (name, owner, description) {
-  const backendRagServer = config.get('backend_rag_service')
   const response = await fetch(backendRagServer + '/knowledge/groups', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -18,6 +31,35 @@ async function createGroup (name, owner, description) {
   return response.json()
 }
 
+async function getGroup (groupId) {
+  const response = await fetch(`${backendRagServer}/knowledge/groups/${groupId}`)
+
+  if (!response.ok) {
+    const errorMessage = await response.text()
+    console.error(`Failed to fetch group ${groupId} with status ${response.status}: ${errorMessage}`)
+    throw Boom.badImplementation()
+  }
+
+  return response.json()
+}
+
+async function addSource (groupId, name, type, location) {
+  const response = await fetch(`${backendRagServer}/knowledge/groups/${groupId}/sources`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, type, location })
+  })
+
+  if (!response.ok) {
+    const errorMessage = await response.text()
+    console.error(`Failed to add source with status ${response.status}: ${errorMessage}`)
+    throw Boom.badImplementation()
+  }
+}
+
 export {
-  createGroup
+  getGroups,
+  createGroup,
+  getGroup,
+  addSource
 }
